8726035b3da9f381481bff0f58bcddcaad1cc606
javier <javiro76@gmail.com>
2023-06-29 12:05:23 -0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import { useState } from 'react'
+import Header from './components/Header'
+
+
+
+
+function App() {
+
+
+  return (
+    
+    <div>
+        <Header></Header>
+        <h1>hola hcao
+        </h1>
+    </div>
+     
+  )
+}
+
+export default App
