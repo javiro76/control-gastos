519cd27fd9045ed3f28c84e6d976f5abe9f54b9a
javier <javiro76@gmail.com>
2023-06-30 18:04:53 -0500

    agregando modal y boton para cerrar

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,7 @@
 import { useState } from 'react'
 import Header from './components/Header'
+import IconoNuevoGasto from './img/nuevo-gasto.svg' 
+import Modal from './components/Modal'
 
 
 
@@ -8,6 +10,12 @@ function App() {
 
   const[presupuesto, setPresupuesto] = useState(0)
   const [isValidPresupuesto, setIsValidPresupuesto] = useState(false);
+
+  const [modal,setModal] = useState(false);
+
+  const handleNuevoGasto = () => {
+    setModal(true);
+  }
 
   return (
     
@@ -19,6 +27,20 @@ function App() {
           setIsValidPresupuesto={setIsValidPresupuesto}
 
         />
+
+        {isValidPresupuesto && 
+        <div className='nuevo-gasto'> 
+            <img
+              src={IconoNuevoGasto}
+              alt='icono nuevo gasto'
+              onClick={handleNuevoGasto}
+            />
+        </div>
+        }
+
+        {modal && <Modal
+                    setModal={setModal}
+                  />}
         
         
     </div>
