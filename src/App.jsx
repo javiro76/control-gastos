9c2f05ad6adffb41f9dd0d48c1f05c49567e0cd0
javier <javiro76@gmail.com>
2023-06-30 16:48:52 -0500

    se realiza programacion de un nuevo componente de presupuesto

--- src/App.jsx
+++ src/App.jsx
@@ -6,11 +6,19 @@ import Header from './components/Header'
 
 function App() {
 
+  const[presupuesto, setPresupuesto] = useState(0)
+  const [isValidPresupuesto, setIsValidPresupuesto] = useState(false);
 
   return (
     
     <div>
-        <Header/>
+        <Header
+          presupuesto={presupuesto}
+          setPresupuesto={setPresupuesto}
+          isValidPresupuesto={isValidPresupuesto}
+          setIsValidPresupuesto={setIsValidPresupuesto}
+
+        />
         
         
     </div>
--- src/components/NuevoPresupuesto.jsx
+++ src/components/NuevoPresupuesto.jsx
@@ -1,16 +1,35 @@
-import React from 'react'
+import {useState} from 'react'
+import Mensaje from './Mensaje';
 
-const NuevoPresupuesto = () => {
+const NuevoPresupuesto = ({presupuesto,setPresupuesto, setIsValidPresupuesto}) => {
+  
+  const [mensaje, setMensaje ] = useState('');
+
+  const handlePresupuesto = (e)=>{
+    e.preventDefault();
+    
+    if(!(presupuesto) || (presupuesto) < 0 ){
+      setMensaje('no es un presupuesto valido')
+      return
+    }
+    setMensaje('');
+    setIsValidPresupuesto(true);
+
+
+  }
+  
   return (
     <div className='contenedor-presupuesto contedor sombra'>
-        <form className='formulario'>
+        <form onSubmit={handlePresupuesto} className='formulario'>
           <div className='campo'>
               <label>Definir Presupuesto</label>
 
               <input
                   className='nuevo-presupuesto'
-                  type='text'
+                  type='number'
                   placeholder='Añade tu presupuesto'
+                  value={presupuesto}
+                  onChange={(e)=>setPresupuesto(Number(e.target.value))}
 
 
               />
@@ -19,7 +38,8 @@ const NuevoPresupuesto = () => {
 
           <input type='submit' value='Añadir'/>
 
-
+          {mensaje && <Mensaje tipo="error">{mensaje}</Mensaje>}
+          
         </form>
     </div>
   )
