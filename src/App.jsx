cd2a944ce58619754778a932ae99a370b0a75791
javier <javiro76@gmail.com>
2023-07-05 18:02:29 -0500

    Agregar formulario de registro de gastos y gestionar sus estados en este commit, se añade un nuevo componente dentro de un modal, se crea añade el gasto, cantidad, y se crean las categorias, con sus respectivos useState

--- src/App.jsx
+++ src/App.jsx
@@ -12,10 +12,16 @@ function App() {
   const [isValidPresupuesto, setIsValidPresupuesto] = useState(false);
 
   const [modal,setModal] = useState(false);
+  const [animarModal,setAnimarModal] = useState(false);
 
   const handleNuevoGasto = () => {
     setModal(true);
+    
+    setTimeout(() => {
+      setAnimarModal(true);
+    }, 500);
   }
+
 
   return (
     
@@ -40,6 +46,8 @@ function App() {
 
         {modal && <Modal
                     setModal={setModal}
+                    animarModal={animarModal}
+                    setAnimarModal={setAnimarModal}
                   />}
         
         
