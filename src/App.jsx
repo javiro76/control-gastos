b9b8baa62d4cbbe952c6d74937647bf4eb03cba7
javier <javiro76@gmail.com>
2023-07-07 10:25:00 -0500

    se creo funcion para formatear fecha

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { useState } from 'react'
 import Header from './components/Header'
+import ListadoGastos from './components/ListadoGastos'
 import Modal from './components/Modal'
 import { generarId } from './helpers'
 import IconoNuevoGasto from './img/nuevo-gasto.svg' 
@@ -27,6 +28,7 @@ function App() {
 
   const guardarGasto = gasto => {
     gasto.id = generarId();
+    gasto.fecha = Date.now();
     setGastos([...gastos, gasto]);
 
     setAnimarModal(false);
@@ -50,13 +52,23 @@ function App() {
         />
 
         {isValidPresupuesto && 
-        <div className='nuevo-gasto'> 
-            <img
-              src={IconoNuevoGasto}
-              alt='icono nuevo gasto'
-              onClick={handleNuevoGasto}
-            />
-        </div>
+        <>
+
+            <main>
+              <ListadoGastos
+                  gastos={gastos}
+              />
+              
+            </main>
+
+            <div className='nuevo-gasto'> 
+                <img
+                  src={IconoNuevoGasto}
+                  alt='icono nuevo gasto'
+                  onClick={handleNuevoGasto}
+                />
+            </div>
+        </>
         }
 
         {modal && <Modal
